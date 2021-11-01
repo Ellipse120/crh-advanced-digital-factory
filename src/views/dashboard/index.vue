782850d9adab8da8792739a8b7c3fcd881b83ac1
路赛 <ellipse120@gmail.com>
2021-11-01 16:03:52 +0800

    refactor: remove unused code

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -6,23 +6,11 @@
 
 <script>
 import Demo from './demo'
-import { useMouse } from '@vueuse/core'
-import { onMounted, ref } from '@vue/composition-api'
 
 export default {
   name: 'Dashboard',
   components: {
     Demo
-  },
-  setup () {
-    // tracks mouse position
-    const { x, y } = useMouse()
-    const tt = ref(null)
-
-    onMounted(() => {
-      console.log('MOUNTED')
-    })
-    return { x, y, tt }
   }
 }
 </script>
